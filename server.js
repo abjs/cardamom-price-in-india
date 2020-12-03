19afd9ea4cba47b8e38b32d7bd43df45d091a6ae
unknown <itsmeabjs@gmail.com>
2020-12-03 15:49:30 +0530

    Simple

--- server.js
+++ server.js
@@ -2,11 +2,11 @@ const express = require('express');
 const app = express();
 require('dotenv').config();
 const port = process.env.PORT || 3000;
-app.use('/price',require('./route/price'));
-app.use('/',require('./route/price'));
+// app.use('/price',require('./route/price'));
+app.use('/', require('./route/price'));
 
 
 
-app.listen(port,(req,res)=>{
+app.listen(port, (req, res) => {
     console.log(`server start on port ${port}`)
 })
