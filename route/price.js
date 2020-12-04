04036867c6aa45f419a7e4e88a79c706f98f5b74
Abin Joseph <itsmeabjs@gmail.com>
2020-12-05 03:20:32 +0530

    Google meta tag

--- route/price.js
+++ route/price.js
@@ -2,18 +2,6 @@ const express = require('express')
 const routes = express.Router()
 const axios = require('axios');
 const cheerio = require('cheerio');
-// axios.get('https://www.indianspices.com/marketing/price/domestic/daily-price.html')
-//     .then((res) => {
-//         const $ = cheerio.load(res.data,);
-//         let list = [];
-//         $('table')
-//             .each((index, element) =>
-//                 list.push($(element).html()))
-//         routes.get('/', function (req, res) {
-//             res.send( `<h1>BY Abin</h1><br><table>${list[1]}</table>` );   
-//         })
-//     })
-
 const fetchData = async (siteUrl) => {
     const result = await axios.get(siteUrl);
     return cheerio.load(result.data);
@@ -31,6 +19,7 @@ routes.get('', async (req, res) => {
     <html lang="en">
     <head>
         <meta charset="UTF-8">
+        <meta name="google-site-verification" content="d2vem5q5DGNV7EWjIUEz8LZUT8XqkNgVwj35Q_DYabo" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta name="title" content="Cardamom Price Today">
         <meta name="description" content="Hear you can find Cardamom Price of every day in kerala">
@@ -60,6 +49,4 @@ routes.get('', async (req, res) => {
     );
 
 })
-
-
 module.exports = routes
