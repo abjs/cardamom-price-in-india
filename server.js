64b4432723c0ffaa93b3cb9d537eac5e7a2115f0
Abin Joseph <itsmeabjs@gmail.com>
2020-12-05 03:41:56 +0530

    Site Map

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const app = express();
+var path = require('path');
 require('dotenv').config();
 const port = process.env.PORT || 3000;
+app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', require('./route/price'));
 app.listen(port, (req, res) => {
     console.log(`server start on port ${port}`)
